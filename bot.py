c007a3f9081969f3597f2fb91cd50bca4ae6e927
HeelMePlz <78705543+HeelMePlz@users.noreply.github.com>
2022-09-28 16:33:11 +0000

    fix syntax error with name

--- bot.py
+++ bot.py
@@ -13,7 +13,7 @@ bot = commands.Bot(command_prefix="!")
 
 @bot.event
 async def on_ready():
-    print(f"{bot.user.name} has connected to Discord!")
+    print(f"{bot.user} has connected to Discord!")
 
 
 @bot.command(name="hello")
